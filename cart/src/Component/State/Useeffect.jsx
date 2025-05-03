455103aa2281336652eac4791a7ccb1f1d906a4d
felix-murugan <felix.murugan@digidense.in>
2025-05-03 11:05:26 +0530

    Update Useeffect.jsx

--- cart/src/Component/State/Useeffect.jsx
+++ cart/src/Component/State/Useeffect.jsx
@@ -1,30 +1,29 @@
-
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './useeffect.css'
 
-const useeffect = () => {
-    const [count, setCount] = useState(100)
-    useEffect(() => {
-        console.log("useEffect is called")
-        const interval = setInterval(() => {
-            setCount((prevCount) => prevCount + 1)
-        }, 1000)
+const Useeffect = () => {
+  const [count, setCount] = useState(100)
 
-        return () => {
-            clearInterval(interval)
-            console.log("useEffect is cleaned up")
-        }
+  useEffect(() => {
+    console.log("useEffect is called")
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+      console.log("useEffect is cleaned up")
     }
-    , [])
+  }, [])
+
   return (
     <div className='useeffect'>
-        <h1>useEffect</h1>
-        <h2 className='count'>Count: {count}</h2>
-        <button onClick={() => setCount(100)} className='btn-1'>setCount</button>
-        <button onClick={() => setCount(0)} className='btn-2'>Reset</button>
+      <h1>useEffect</h1>
+      <h2 className='count'>Count: {count}</h2>
+      <button onClick={() => setCount(100)} className='btn-1'>setCount</button>
+      <button onClick={() => setCount(0)} className='btn-2'>Reset</button>
     </div>
   )
 }
-export default useeffect
+
+export default Useeffect
