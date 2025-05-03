74d8558eca4e7aa5ca19bac56208d3d4d72e5007
felix-murugan <felix.murugan@digidense.in>
2025-05-03 11:31:46 +0530

    Update App.jsx

--- cart/src/App.jsx
+++ cart/src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
-import Carts from './Component/State/Carts'
-import Useref from './Component/State/Useref'
-import Useeffect from './Component/State/Useeffect'
+import './App.css'; // CSS for the App component
+import Carts from './Component/State/Carts'; // Import the Carts component
+import Useref from './Component/State/Useref'; // Import the Useref component
+import Useeffect from './Component/State/Useeffect'; // Import the Useeffect component
 
 function App() {
   return (
     <div>
+      {/* Uncomment the components below to use them */}
       {/* <Carts /> */}
       {/* <Useeffect /> */}
-      <Useref />
+      <Useref /> {/* This component is currently being used */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
