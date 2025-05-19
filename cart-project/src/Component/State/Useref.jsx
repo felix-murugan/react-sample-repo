dde2a9459425fe7190bb0b9e005e7c278ca774aa
felix-murugan <felix.murugan@digidense.in>
2025-05-19 13:50:30 +0530

    Update Useref.jsx

--- cart-project/src/Component/State/Useref.jsx
+++ cart-project/src/Component/State/Useref.jsx
@@ -13,7 +13,6 @@ function PreviousCountExample() {
   const prevCount = prevCountRef.current;
 
   return (
-    
     <div className="p-4">
       <p className="text-lg">Current Count: {count}</p>
       <p className="text-gray-600">Previous Count: {prevCount}</p>
